7efa10c570b3bdf0b4e04f7a8e315d2c500cec01
ctada <celine.ta@students.olin.edu>
2015-03-04 13:21:06 -0500

    minimal attempt to check amueller's wordcloud

---
+++ wordcloud_ctaAttempt1.py
@@ -0,0 +1,15 @@
+"""Minimal attempt to check wordcloud"""
+from os import path
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+d = path.dirname(__file__)
+
+# Read the whole text.
+text = open(path.join(d, 'constitution.txt')).read()
+wordcloud = WordCloud().generate(text)
+# Open a plot of the generated image.
+plt.imshow(wordcloud)
+plt.axis("off")
+plt.show()
+

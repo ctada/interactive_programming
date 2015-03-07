65a059092951fe5505ef6264f1f730fcdaf5d499
Kelly Brennan <kelly.brennan@students.olin.edu>
2015-03-07 18:46:12 -0500

    Adding GDP Data

--- Bokeh_Intro/Bokeh_Test1.py
+++ Bokeh_Intro/Bokeh_Test1.py
@@ -2,7 +2,7 @@
 Testing Bokeh
 """
 
-from bokeh.sampledata import us_states, us_counties
+from bokeh.sampledata import us_states
 from bokeh.plotting import *
 
-#output_file("world_map.html", title = "Wold Map")
+output_file("world_map.html", title = "Wold Map")

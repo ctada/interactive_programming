802adbe739aa69ee6b6c1d5357b328847332250f
ctada <celine.ta@students.olin.edu>
2015-03-07 19:47:01 -0500

    now with state GDP data

--- interactive_visualization.py
+++ interactive_visualization.py
@@ -10,6 +10,7 @@ import numpy as np
 from bokeh.models import HoverTool as hov
 import pandas as pd
 from bokeh.charts import Scatter
+from collections import OrderedDict
 
 class Display_Map():
 	"""
@@ -22,24 +23,43 @@ class Display_Map():
 		Initializes map image, borders, countries
 		"""
 		#for now, data is not related to geography
-		stats = pd.read_csv('data/happiness_UScentric.csv')
-		print stats
+		#stats = pd.read_csv('data/happiness_UScentric.csv')
 		#self.states = stats['What state or province do you live in, if applicable?']
 		#self.year = stats['Do you love and appreciate yourself?']
 		#self.safety = stats['Are your surroundings physically safe?']
-		df = stats[["Do you love and appreciate yourself?", "Are your surroundings physically safe?", "What state or province do you live in, if applicable?"]]
-		g = df.groupby('What state or province do you live in, if applicable?')
+		#df = stats[["Do you love and appreciate yourself?", "Are your surroundings physically safe?", "What state or province do you live in, if applicable?"]]
+		#g = df.groupby('What state or province do you live in, if applicable?')
+		#self.pdict = OrderedDict()
 
-		scatter = Scatter(g, filename="state_happiness.html", title="State Happiness GroupBy")
-		scatter.show()
+		#for i in g.groups.keys():
+		#    labels = g.get_group(i).columns
+		#    xname = labels[0]
+		#    yname = labels[1]
+		#    x = getattr(g.get_group(i), xname)
+		#    y = getattr(g.get_group(i), yname)
+		#    pdict[i] = zip(x, y)
+
+		# any of the following commented are valid Scatter inputs
+		#xyvalues = pdict
+		#xyvalues = pd.DataFrame(xyvalues)
+		#xyvalues = xyvalues.values()
+		#xyvalues = np.array(xyvalues.values())
+
 		#self.juvRate=[]
 		#for rate in stats['Rate per 100,000']:
 		#	if not np.isnan(rate):
 		#		self.juvRate.extend(int(rate))
+		
+		#TOOLS="resize,crosshair,pan,wheel_zoom,box_zoom,reset,previewsave"
+		#scatter = Scatter(
+		#    self.pdict, filename="state_happiness.html", tools=TOOLS, ylabel='Physical Safety'
+		#)
+		#scatter = Scatter(g, filename="state_happiness.html", title="State Happiness GroupBy")
+		#scatter.show()
 
 
-		df = pd.read_csv('GDP_per_state.csv', names = ['State', 'GDP'])
-		state_GDP = dict(zip(df.State, df.GDP))
+		df = pd.read_csv('data/GDP_per_state.csv', names = ['State', 'GDP'])
+		self.state_GDP = dict(zip(df.State, df.GDP))
 
 
 	def lookup_country(self,hover_pos):
@@ -58,18 +78,24 @@ class Display_Map():
 		"""
 		bk.output_file("Map_bk.html", title="Hello World!")  # save plot as html
 		fig = bk.figure(plot_width = 600, plot_height= 600, title = "Map") #creates new Bokeh plot
-		fig.circle(#self.gender, self.year,
+		#fig.circle(#self.gender, self.year,
          #size=(self.juvRate), # px
-         xs, ys,
-         size =2,
-         fill_alpha=0.5,
-         fill_color="steelblue",
-         line_alpha=0.8,
-         line_color="crimson")
+         #xs, ys,
+         #size =2,
+         #fill_alpha=0.5,
+         #fill_color="steelblue",
+         #line_alpha=0.8,
+         #line_color="crimson")
 		#fig.circle(xs,ys)
 
-
 		#fig.line(xs, ys, line_width=2)
+
+		#TOOLS="resize,crosshair,pan,wheel_zoom,box_zoom,reset,previewsave"
+		#scatter = Scatter(
+		#    self.state_GDP, filename="state_happiness.html", tools=TOOLS, ylabel='State GDP'
+		#)
+		#scatter.show()
+
 		bk.save(obj=fig)
 		bk.show(fig)
 

4f09f8fd17cd3bd6571da41df70fd94e459f7569
ctada <celine.ta@students.olin.edu>
2015-03-10 23:09:12 -0400

    temporary stash of oop version work

--- Interactive_Visualization_Map.py
+++ Interactive_Visualization_Map.py
@@ -11,11 +11,12 @@ import bokeh.plotting as bk
 import numpy as np
 from bokeh.models import HoverTool as hov
 import pandas as pd
+from collections import OrderedDict
 import pdb
 
-class Display_Map():
+class Display_Map(object):
 	"""
-	Takes in map image, array of defining coordinates for each country, and list of countries. Upon a mouse action, Display_Map will
+	Takes in map image, array of defining coordinates for each State, and list of countries. Upon a mouse action, Display_Map will
 	look up location of mouse action in relation to map and update display accordingly.
 	"""
 	#def __init__(self,image, borders, list_countries):
@@ -60,36 +61,28 @@ class Display_Map():
 		self.state_GDP = dict(zip(df.State, df.GDP))
 
 		stats = pd.read_csv('data/happiness_UScentric.csv')
-		self.states = stats['What state or province do you live in, if applicable?']
-		self.happy = stats['Do you love and appreciate yourself?']
-		self.safety = stats['Are your surroundings physically safe?']
+		data_states = stats['What state or province do you live in, if applicable?']
+		data_happy = stats['Do you love and appreciate yourself?']
+		data_safety = stats['Are your surroundings physically safe?']
 
-		self.state_hap = {}
-		self.state_safe = {}
-		# average happiness per state
-		for i in range(len(self.states)):
-			tryState = self.states[i]
-			if tryState in self.state_hap: #then they should also be in self.state_safe
-				self.state_hap[tryState].append(self.happy[i])
-				self.state_safe[tryState].append(self.safety[i])
-			#elif type(tryState) is 'str': # filter out nan types
-			else:
-				self.state_hap[tryState]=[self.happy[i]]
-				self.state_safe[tryState]=[self.safety[i]]
+		self.states = {} # create master dictionary of State objects
+		
+		# fill in self.states with State objects with their own happiness and safety ratings
+		for i in range(len(data_states)):
+			tryState =	data_states[i]
+			if tryState in self.state_GDP and tryState in boundary_data.keys(): #ensures territory is in US and we have info for it
+				if tryState in self.states: #then they should also be in self.state_safe
+					self.states[tryState].add_happiness_safety(data_happy[i], data_safety[i])				
+				else:
+					boundariesX = boundary_data[tryState]['lons']
+					boundariesY = boundary_data[tryState]['lats']
+					self.states[tryState] = State(tryState, data_happy[i], data_safety[i], self.state_GDP[tryState], boundariesX, boundariesY)
 
-		self.avgHap= [(i,float(sum(v))/len(v)) for i,v in self.state_hap.items()]
-		self.avgSafe= [(i,float(sum(v))/len(v)) for i,v in self.state_safe.items()]
+		# averages Happiness and Safety ratings for each State on master dictionary
+		for k in self.states.keys():
+			self.states[k].average_data()
 
-	def lookup_country(self,hover_pos):
-		"""Given position, finds Country object
-			returns: country (Country object)
-		"""
-		pass
-	def update(self,hover_pos):
-		"""
-		Updates map display upon mouse action
-		"""
-		pass
+
 	def run_display(self):
 		"""
 		Maintains display/ interaction experience
@@ -99,32 +92,114 @@ class Display_Map():
 
 		TOOLS = "pan, wheel_zoom, box_zoom, reset, hover"
 
+		# Pull info from each State in self.states for use in bokeh
+		names = []
+		happiness = []
+		safety = []
+		grossDP = []
+		state_xs = []
+		state_ys = []
+
+		for elem in self.states.keys():
+			state_obj = self.states[elem]
+			names.append(state_obj.get_name())
+			happiness.append(state_obj.get_happiness())
+			safety.append(state_obj.get_safety())
+			grossDP.append(state_obj.get_GDP)
+			state_xs.append(state_obj.getX())
+			state_ys.append(state_obj.getY())
+
+		# TODO: IS THERE ANY WAY TO GET OBJECT INFO INTO PATCHES OR TOOLTIPS?
+		source = bk.ColumnDataSource(
+	    	data=dict(
+		        name=names,
+		        happiness=happiness,
+		        safety=safety,
+		        GDP=grossDP,
+		        state_xs = state_xs,
+		        state_ys = state_ys
+   			 )
+		)
+
 		bk.output_file("Map_bk.html", title="Hello World!")  # save plot as html
 		fig = bk.figure(plot_width = 600, plot_height= 600, title = "Map", tools = TOOLS) #creates new Bokeh plot
 
-		fig.patches (self.state_xs, self.state_ys,
+		fig.patches (state_xs, state_ys,
 			fill_color = state_colors, fill_alpha = 0.7, 
 			line_color = "black", line_width = 0.5)
+		#pdb.set_trace()
+
+		#fig.text(self.state_xs, self.state_ys, # can't recognize coordinates as value on chart
+    	#	text=self.states.keys(),text_color="#333333",
+    	#	text_align="center", text_font_size="10pt")
+
+		hover = fig.select(dict(type = hov))
+		# hover.snap_to_data = False
+		hover.tooltips = OrderedDict([("State", "@names"), ("GDP", "@GDP")])
 
 		bk.save(obj=fig)
 		bk.show(fig)
 
-class Country():
-	def __init__(self,name, borders, happiness, GDP):
+class State():
+	def __init__(self,name,happiness, safety, GDP, borderX, borderY):
 		"""
-		Creates Country object with name, borders, mean happiness level, and GDP of a certain year
+		Creates State object with name, mean happiness level, and contribution to GDP of a certain year
 		"""
-		pass
+		self.name = name
+		self.happiness = [happiness]
+		self.safety = [safety]
+		self.GDP = GDP
+		self.borderX = borderX
+		self.borderY = borderY
+
+	def get_name(self):
+		"""returns name"""
+		return self.name
+
 	def get_happiness(self):
 		"""
-		returns country's mean happiness level
+		returns State's mean happiness level
 		"""
-		pass
+		return self.happiness
+
 	def get_GDP(self):
 		"""
-		returns country's GDP
+		returns State's contribution to GDP
 		"""
-		pass
+		return self.GDP
+
+	def get_safety(self):
+		"""
+		returns safety level
+		"""
+		return self.safety
+
+	def getX(self):
+		"""
+		returns x-position perimeter coordinates
+		"""
+		return self.borderX
+
+	def getY(self):
+		"""
+		returns y-position perimeter coordinates
+		"""
+		return self.borderY
+
+	def add_happiness_safety(self, hapVal, safeVal):
+		"""
+		No return. Records happiness and safety rating to State object.
+		"""
+		self.happiness.append(hapVal)
+		self.safety.append(safeVal)
+
+	def average_data(self):
+		""" 
+		No return. Averages a state's happiness, safety
+		"""
+		self.happiness = float(sum(self.happiness))/len(self.happiness)
+		self.safety = float(sum(self.safety))/len(self.safety)
+
 
 class Interactive():
 	def __init__(self):

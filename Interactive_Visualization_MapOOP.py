b73230f6bfb24dd25abef2f768857eeede33402e
ctada <celine.ta@students.olin.edu>
2015-03-11 22:39:22 -0400

    clean up code

--- Interactive_Visualization_MapOOP.py
+++ Interactive_Visualization_MapOOP.py
@@ -24,12 +24,11 @@ class Display_Map(object):
 		"""
 		Initializes map image, borders, countries
 		"""
-
-
+		# pull in geographical data
 		self.state_xs = [state_boundaries.data[code]['lons'] for code in state_boundaries.data]
 		self.state_ys = [state_boundaries.data[code]['lats'] for code in state_boundaries.data]
 
-		# Read data from CSV file
+		# Read happiness and safety data from CSV file
 		df = pd.read_csv('data/GDP_per_state.csv', names = ['State', 'GDP'])
 		self.state_GDP = dict(zip(df.State, df.GDP))
 
@@ -38,36 +37,30 @@ class Display_Map(object):
 		data_happy = stats['Do you love and appreciate yourself?']
 		data_safety = stats['Are your surroundings physically safe?']
 
-		# self.states = dict(zip(self.state_GDP.keys(), [State(self.state_GDP.keys()[i],0,0,0,0,0) for i in range(len(self.state_GDP.keys()))])) # create master dictionary of State objects
+		# Create dictionary of states that we want to display on map, with state name (string) as a key and State object as the value
 		self.states = dict(zip(state_boundaries.data.keys(), [State(state_boundaries.data.keys()[i],0,0, self.state_GDP[name], state_boundaries.data[name]['lons'], state_boundaries.data[name]['lats']) for i,name in enumerate(state_boundaries.data.keys())]))
-		# fill in self.states with State objects with their own happiness and safety ratings
+		
+		# Add happiness and safety ratings to their corresponding States
 		for i in range(len(data_states)):
 			tryState =	data_states[i]
 			if tryState in self.states and tryState in state_boundaries.data:
 				self.states[tryState].add_happiness_safety(data_happy[i], data_safety[i])			
-		# print self.states
+
 		# averages Happiness and Safety ratings for each State on master dictionary
-		#for k in self.states.keys():
 		for v in self.states.values():
-			if len(v.happiness) >= 2: #checking we got at least one actual entry
+			if len(v.happiness) >= 2: #checking we got at least one actual entry besides the initial 0
 				v.average_data()
-
-		# print self.states.values()
-		# for i in self.states.values():
-		# 	print i
-		# print getattr(self.states.values(), 'get_GDP')
-
 
 	def run_display(self):
 		"""
 		Maintains display/ interaction experience
 		"""
 		colors = ["#F1EEF6", "#D4B9DA", "#C994C7", "#DF65B0", "#DD1C77", "#980043"]
-		state_colors = ["#F1EEF6"] #NEEDS TO BE SET IN LATER CODE
+		state_colors = ["#4C0B5F"] # color of state when mouse is hovering over it
 
 		TOOLS = "pan, wheel_zoom, box_zoom, reset, hover"
 
-		# Pull info from each State in self.states for use in bokeh
+		# Pull info from each State into attribute-specific lists for use in bokeh
 		names = []
 		happiness = []
 		safety = []
@@ -75,7 +68,6 @@ class Display_Map(object):
 		xs = []
 		ys = []
 
-		# print len(self.states.keys())
 		for st in self.states.values():
 			if type(st) is not str:
 				names.append(st.name)
@@ -85,23 +77,10 @@ class Display_Map(object):
 				xs.append(st.borderX)
 				ys.append(st.borderY)
 
-		# TODO: IS THERE ANY WAY TO GET OBJECT INFO INTO PATCHES OR TOOLTIPS?
-		# source = bk.ColumnDataSource(
-	 #    	data=dict(
-		#         name=names,
-		#         happiness=happiness,
-		#         safety=safety,
-		#         GDP=grossDP,
-		#         state_xs = state_xs,
-		#         state_ys = state_ys
-  #  			 )
-		# )
-		# print len(names)
-		print names
-
 		bk.output_file("Map_bk.html", title="Hello World!")  # save plot as html
 		fig = bk.figure(plot_width = 600, plot_height= 600, title = "Map", tools = TOOLS) #creates new Bokeh plot
 
+		#display patches to match state shape, assigning state name, happiness, and GDP to each their respective patch
 		fig.patches (xs, ys,
 			fill_color = state_colors, fill_alpha = 0.7, 
 			source = bk.ColumnDataSource(data = {
@@ -111,23 +90,11 @@ class Display_Map(object):
 				}),
 			line_color = "black", line_width = 0.5)
 
-		# fig.patches (state_xs, state_ys,
-		# 	fill_color = state_colors, fill_alpha = 0.7, 
-		# 	line_color = "black", line_width = 0.5)
-		#pdb.set_trace()
-
-		#fig.text(self.state_xs, self.state_ys, # can't recognize coordinates as value on chart
-    	#	text=self.states.keys(),text_color="#333333",
-    	#	text_align="center", text_font_size="10pt")
-
-		hover = fig.select(dict(type = hov))
+		hover = fig.select(dict(type = hov)) # turn on hovering
 		# hover.snap_to_data = False
-		hover.tooltips = OrderedDict([("State", "@state"), ("Happiness", "@happiness"), ("GDP", "@GDP")])#, ("GDP", "@GDP")])
-				# hover.snap_to_data = False
-
-		# hover.tooltips = ([('State:', '@state'), ("(x,y)", "($x, $y)")], [('Happiness', '@happiness')])
-
-		bk.save(obj=fig)
+		hover.tooltips = OrderedDict([("State", "@state"), ("Happiness", "@happiness"), ("GDP", "@GDP")]) # Customize what is displayed in tooltips
+		
+		bk.save(obj=fig) 
 		bk.show(fig)
 
 class State():
@@ -136,14 +103,14 @@ class State():
 		Creates State object with name, mean happiness level, and contribution to GDP of a certain year
 		"""
 		self.name = name
-		self.happiness = [happiness]
-		self.safety = [safety]
+		self.happiness = [happiness] #create list with happiness as the first entry
+		self.safety = [safety] #create list with safety as the first entry
 		self.GDP = GDP
 		self.borderX = borderX
 		self.borderY = borderY
 
 	def __str__(self):
-		return self.name + str(self.happiness) + "," + str(self.safety) + "," + str(self.GDP) + str(self.borderX) + str(self.borderY)
+		return self.name + str(self.happiness) + "," + str(self.safety) + "," + str(self.GDP) + "," + str(self.borderX) + "," + str(self.borderY)
 
 	def add_happiness_safety(self, hapVal, safeVal):
 		"""
@@ -159,25 +126,7 @@ class State():
 		self.happiness = float(sum(self.happiness))/(len(self.happiness)-1) # subtract one from denominator to get remove influence of initial State object set to 0 (added 0 happiness, safety to State's record)
 		self.safety = float(sum(self.safety))/(len(self.safety)-1)
 
-
-class Interactive():
-	def __init__(self):
-		"""
-		Initializes monitoring of user input
-		"""
-		hover = fig.select(dict(type = HoverTool))
-		# hover.snap_to_data = False
-		hover.tooltips = [("(x,y)", "($x, $y)")]
-
-		show(fig)
-
-	def get_mouse_position(self):
-		"""
-		returns mouse position in relation to map image (translate from screen position)
-		"""
-		pass
 if __name__ == '__main__':
-	vis = Display_Map() # pass in arguments
-	#mouse = Interactive() 
+	vis = Display_Map() 
 	vis.run_display()
 
